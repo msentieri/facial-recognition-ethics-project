d8bcbac8933feb564ae3122540c1cac0a931af32
msentieri <msentieri@outlook.com>
2025-04-25 12:26:37 -0500

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -238,7 +238,7 @@
                       <svg class="h-4 w-4 text-indigo-500 mt-0.5 mr-2 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                       </svg>
-                      <span class="text-sm">Demo uses TensorFlow.js face detection model</span>
+                      <span class="text-sm">Demo uses DeepFace library for facial verification</span>
                     </li>
                     <li class="flex items-start">
                       <svg class="h-4 w-4 text-indigo-500 mt-0.5 mr-2 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
